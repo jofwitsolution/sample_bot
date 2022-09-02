2e589689a1097df3f8561796cc7a9a6c862fbc6f
Faleye Oluwafemi joseph <faleyeoluwafemi1@gmail.com>
2022-09-02 22:04:01 +0100

    index cganged

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ const TOKEN = process.env.TOKEN;
 
 const bot = new telegramBot(TOKEN, { polling: true });
 
-// bot.on("message", (msg) => {
-//   const keyword = "jofwitsampledemo_bot";
+bot.on("message", (msg) => {
+  const keyword = "jofwitsampledemo_bot";
 
-//   const text = msg.text?.toString().toLowerCase().trim();
+  const text = msg.text?.toString().toLowerCase().trim();
 
-//   if (!text) return;
+  if (!text) return;
 
-//   const { id } = msg.chat;
-//   const { username } = msg.from;
+  const { id } = msg.chat;
+  const { username } = msg.from;
 
-//   if (text.includes(keyword)) {
-//     bot
-//       .sendMessage(id, `Hello ${username}!\n How may I help you?`)
-//       .then((res) => console.log("success"))
-//       .catch((error) => console.log(error));
-//   }
-// });
+  if (text.includes(keyword)) {
+    bot
+      .sendMessage(id, `Hello ${username}!\n How may I help you?`)
+      .then((res) => console.log("success"))
+      .catch((error) => console.log(error));
+  }
+});
 
 bot.on("message", (msg) => {
   const keyword = "hi";
@@ -47,80 +47,80 @@ bot.on("message", (msg) => {
   }
 });
 
-// bot.on("message", (msg) => {
-//   const keyword = "thank you";
+bot.on("message", (msg) => {
+  const keyword = "thank you";
 
-//   const text = msg.text?.toString().toLowerCase().trim();
+  const text = msg.text?.toString().toLowerCase().trim();
 
-//   if (!text) return;
+  if (!text) return;
 
-//   const { id } = msg.chat;
-//   const { username } = msg.from;
+  const { id } = msg.chat;
+  const { username } = msg.from;
 
-//   if (
-//     text.startsWith(keyword) ||
-//     text.indexOf(keyword) === 0 ||
-//     text.endsWith(keyword)
-//   ) {
-//     bot
-//       .sendMessage(id, `Welcome dear ${username}`)
-//       .then((res) => console.log("success"))
-//       .catch((error) => console.log(error));
-//   }
-// });
+  if (
+    text.startsWith(keyword) ||
+    text.indexOf(keyword) === 0 ||
+    text.endsWith(keyword)
+  ) {
+    bot
+      .sendMessage(id, `Welcome dear ${username}`)
+      .then((res) => console.log("success"))
+      .catch((error) => console.log(error));
+  }
+});
 
-// bot.on("message", (msg) => {
-//   const keyword = "bye";
-//   //   console.log(msg);
-//   const text = msg.text?.toString().toLowerCase();
+bot.on("message", (msg) => {
+  const keyword = "bye";
+  //   console.log(msg);
+  const text = msg.text?.toString().toLowerCase();
 
-//   if (!text) return;
+  if (!text) return;
 
-//   const { id } = msg.chat;
-//   const { username } = msg.from;
+  const { id } = msg.chat;
+  const { username } = msg.from;
 
-//   if (text.startsWith(keyword) || text.includes(keyword)) {
-//     bot
-//       .sendMessage(id, `${username}, hope to see you around again, Bye.`)
-//       .then((res) => console.log("success"))
-//       .catch((error) => console.log(error));
-//   }
-// });
+  if (text.startsWith(keyword) || text.includes(keyword)) {
+    bot
+      .sendMessage(id, `${username}, hope to see you around again, Bye.`)
+      .then((res) => console.log("success"))
+      .catch((error) => console.log(error));
+  }
+});
 
-// bot.on("message", (msg) => {
-//   const keyword = "fuck";
-//   const text = msg.text?.toString().toLowerCase();
+bot.on("message", (msg) => {
+  const keyword = "fuck";
+  const text = msg.text?.toString().toLowerCase();
 
-//   if (!text) return;
+  if (!text) return;
 
-//   const { id } = msg.chat;
-//   const message_id = msg.message_id;
+  const { id } = msg.chat;
+  const message_id = msg.message_id;
 
-//   if (text.startsWith(keyword) || text.includes(keyword)) {
-//     // console.log(msg);
-//     bot
-//       .deleteMessage(id, message_id)
-//       .then((res) => console.log("success"))
-//       .catch((error) => console.log(error));
-//   }
-// });
+  if (text.startsWith(keyword) || text.includes(keyword)) {
+    // console.log(msg);
+    bot
+      .deleteMessage(id, message_id)
+      .then((res) => console.log("success"))
+      .catch((error) => console.log(error));
+  }
+});
 
-// bot.on("message", (msg) => {
-//   const text = msg.text?.toString();
+bot.on("message", (msg) => {
+  const text = msg.text?.toString();
 
-//   if (!text) return;
+  if (!text) return;
 
-//   const { id } = msg.chat;
-//   const message_id = msg.message_id;
+  const { id } = msg.chat;
+  const message_id = msg.message_id;
 
-//   if (containUrl(text)) {
-//     // console.log(msg);
-//     bot
-//       .deleteMessage(id, message_id)
-//       .then((res) => console.log("success"))
-//       .catch((error) => console.log(error));
-//   }
-// });
+  if (containUrl(text)) {
+    // console.log(msg);
+    bot
+      .deleteMessage(id, message_id)
+      .then((res) => console.log("success"))
+      .catch((error) => console.log(error));
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 
